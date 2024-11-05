4b13a31c53923916aeb2840a0517fd20f77dc013
hagenzhang <64102519+hagenzhang@users.noreply.github.com>
2024-11-05 04:12:50 -0500

    completed lab4

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useLocation, useParams } from "react-router";
+import { useLocation } from "react-router";
 import * as db from "../../Database";
 
 
--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -32,11 +32,24 @@ import Square from "./Square";
 import Highlight from "./Highlight";
 import PathParameters from './PathParameters';
 
+import { useSelector } from "react-redux";
+
 
 export default function Lab3() {
+    const { todos } = useSelector((state: any) => state.todosReducer);
+
     return (
         <div id="wd-lab3">
             <h3>Lab 3</h3>
+
+            <ul className="list-group">
+                {todos.map((todo: any) => (
+                    <li className="list-group-item" key={todo.id}>
+                        {todo.title}
+                    </li>
+                ))}
+            </ul>
+            <hr />
 
             <VariablesAndConstants />
             <VariableTypes />
@@ -77,16 +90,8 @@ export default function Lab3() {
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
                 vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
             </Highlight>
-            
+
             <PathParameters />
-
-
-
-
-
-
-
-
 
         </div>
     );
