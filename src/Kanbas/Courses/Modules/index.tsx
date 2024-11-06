e2a258133e57eff314770213b45ec90a4866e690
hagenzhang <64102519+hagenzhang@users.noreply.github.com>
2024-11-05 23:23:10 -0500

    up to 3.4

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ModulesControls from "./ModulesControls";
-import { BsGripVertical } from "react-icons/bs"; // Import the BsGripVertical component from react-icons/bs
+import { BsGripVertical } from "react-icons/bs";
 import ModuleControlButtons from "./ModuleControlButtons";
 import LessonControlButtons from "./LessonControlButtons";
 import { useParams } from "react-router";
+import { addModule, editModule, updateModule, deleteModule }
+    from "./reducer";
+import { useSelector, useDispatch } from "react-redux";
 import * as db from "../../Database";
 
 export default function Modules() {
     const { cid } = useParams();
-    const modules = db.modules;
+    const [moduleName, setModuleName] = useState("");
+    const { modules } = useSelector((state: any) => state.modulesReducer);
+    const dispatch = useDispatch();
 
     return (
         <div>
-            <ModulesControls />
+            <ModulesControls
+                setModuleName={setModuleName}
+                moduleName={moduleName}
+                addModule={() => {
+                    dispatch(addModule({ name: moduleName, course: cid }));
+                    setModuleName("");
+                }} />
             <br />
             <br />
             <br />
@@ -25,8 +36,23 @@ export default function Modules() {
                         <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
                             <div className="wd-title p-3 ps-2 bg-secondary">
                                 <BsGripVertical className="me-2 fs-3" />
-                                {module.name}
-                                <ModuleControlButtons />
+                                {!module.editing && module.name}
+                                {module.editing && (
+                                    <input className="form-control w-50 d-inline-block"
+                                        onChange={(e) => dispatch(updateModule({ ...module, name: e.target.value }))}
+                                        onKeyDown={(e) => {
+                                            if (e.key === "Enter") {
+                                                dispatch(updateModule({ ...module, editing: false }));
+                                            }
+                                        }}
+                                        defaultValue={module.name} />
+                                )}
+                                <ModuleControlButtons
+                                    moduleId={module._id}
+                                    deleteModule={(moduleId) => {
+                                        dispatch(deleteModule(moduleId));
+                                    }}
+                                    editModule={(moduleId) => dispatch(editModule(moduleId))} />
                             </div>
 
                             {module.lessons && (
