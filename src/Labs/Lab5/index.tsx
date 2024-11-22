9af6fd37f2f9ed39bb63a4b3972067e833fe2406
hagenzhang <64102519+hagenzhang@users.noreply.github.com>
2024-11-22 04:28:07 -0500

    until 3.4

---
+++ src/Labs/Lab5/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import EnvironmentVariables from "./EnvironmentVariables";
+import PathParameters from "./PathParameters";
+import QueryParameters from "./QueryParameters";
+import WorkingWithObjects from "./WorkingWithObjects";
+
+export default function Lab5() {
+    return (
+        <div id="wd-lab5">
+            <h2>Lab 5</h2>
+            <div className="list-group">
+                <a href="http://localhost:4000/lab5/welcome"
+                    className="list-group-item">
+                    Welcome
+                </a>
+            </div><hr />
+
+            <EnvironmentVariables />
+            <PathParameters />
+            <QueryParameters />
+            <WorkingWithObjects />
+        </div>
+    );
+}
