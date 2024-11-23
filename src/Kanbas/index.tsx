f02efe61f3fa359dae657346b305e56869d83e9b
hagenzhang <64102519+hagenzhang@users.noreply.github.com>
2024-11-23 05:40:22 -0500

    completed until 4.6

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,7 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Routes, Route, Navigate } from 'react-router';
 import "./styles.css";
-import * as db from "./Database";
 import Account from './Account';
 import Dashboard from './Dashboard';
 import KanbasNavigation from './Navigation';
@@ -9,21 +8,40 @@ import Courses from './Courses';
 import ProtectedRoute from "./Account/ProtectedRoute";
 
 import Session from "./Account/Session";
+import * as userClient from "./Account/client";
+import * as courseClient from "./Courses/client";
+import { useSelector } from "react-redux";
 
 
 export default function Kanbas() {
-    const [courses, setCourses] = useState<any[]>(db.courses);
+    const [courses, setCourses] = useState<any[]>([]);
+    const { currentUser } = useSelector((state: any) => state.accountReducer);
+    const fetchCourses = async () => {
+        try {
+            const courses = await userClient.findMyCourses();
+            setCourses(courses);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    useEffect(() => {
+        fetchCourses();
+    }, [currentUser]);
+
     const [course, setCourse] = useState<any>({
         _id: "1234", name: "New Course", number: "New Number",
         startDate: "2023-09-10", endDate: "2023-12-15", description: "New Description",
     });
-    const addNewCourse = () => {
-        setCourses([...courses, { ...course, _id: new Date().getTime().toString() }]);
+    const addNewCourse = async () => {
+        const newCourse = await userClient.createCourse(course);
+        setCourses([...courses, newCourse]);
     };
-    const deleteCourse = (courseId: any) => {
+    const deleteCourse = async (courseId: string) => {
+        const status = await courseClient.deleteCourse(courseId);
         setCourses(courses.filter((course) => course._id !== courseId));
     };
-    const updateCourse = () => {
+    const updateCourse = async () => {
+        await courseClient.updateCourse(course);
         setCourses(
             courses.map((c) => {
                 if (c._id === course._id) {
